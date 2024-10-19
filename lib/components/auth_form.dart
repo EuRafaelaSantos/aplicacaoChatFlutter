eaa32e4b8f1d8896bb37ce66e5ac7f9f241bce49
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-10-19 17:02:33 -0400

    Validando os dados

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -13,7 +13,10 @@ class _AuthFormState extends State<AuthForm> {
   final _formData = AuthFormData();
 
   void _submit() {
-    _formKey.currentState?.validate();
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) {
+      return;
+    }
   }
 
   @override
@@ -32,12 +35,26 @@ class _AuthFormState extends State<AuthForm> {
                     initialValue: _formData.name,
                     onChanged: (name) => _formData.name = name,
                     decoration: const InputDecoration(labelText: 'Usuário'),
+                    validator: (_name) {
+                      final name = _name ?? '';
+                      if (name.trim().length < 3) {
+                        return 'Necessário NOME com no mínimo 6 caracteres';
+                      }
+                      return null;
+                    },
                   ),
                 TextFormField(
                   key: const ValueKey('email'),
                   initialValue: _formData.email,
                   onChanged: (email) => _formData.email = email,
                   decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (_email) {
+                    final email = _email ?? '';
+                    if (!email.contains('@')) {
+                      return 'EMAIL inválido!';
+                    }
+                    return null;
+                  },
                 ),
                 TextFormField(
                   key: const ValueKey('password'),
@@ -45,6 +62,13 @@ class _AuthFormState extends State<AuthForm> {
                   onChanged: (password) => _formData.password = password,
                   obscureText: true,
                   decoration: const InputDecoration(labelText: 'Senha'),
+                  validator: (_password) {
+                    final password = _password ?? '';
+                    if (password.length < 6) {
+                      return 'Necessário SENHA com no mínimo 6 caracteres';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 12),
                 ElevatedButton(
