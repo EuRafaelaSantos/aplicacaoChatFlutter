00735d31bc4e736341b0ca6f52d02ed11e2d1add
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-10-28 18:04:15 -0400

    Modelo do Chat Message

---
+++ lib/core/models/chat_message.dart
@@ -0,0 +1,19 @@
+class ChatMessage {
+  // dados da menssagem
+  final String id;
+  final String text;
+  final DateTime createAt;
+  //dados do usuário
+  final String userId;
+  final String userName;
+  final String userImageURL;
+
+  const ChatMessage({
+    required this.text,
+    required this.createAt,
+    required this.userId,
+    required this.userName,
+    required this.userImageURL,
+    required this.id,
+  });
+}
