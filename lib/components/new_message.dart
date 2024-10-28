a67962d55bb34ceb44a5a9493f5a8f486c15125a
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-10-28 17:48:28 -0400

    Criando os componentes do Chat

---
+++ lib/components/new_message.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatelessWidget {
+  const NewMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        ' Nova Menssagem',
+      ),
+    );
+  }
+}
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_message.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,20 +9,45 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Chat page'),
-            TextButton(
-              onPressed: () {
-                AuthService().logout();
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
+          title: Text('Chat Flutter'),
+          actions: [
+            DropdownButton(
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(Icons.exit_to_app),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('Sair')
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.black,
+              ),
+              onChanged: (String? value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
               },
-              child: Text('Sair'),
-            ),
+            )
           ],
         ),
-      ),
-    );
+        body: SafeArea(
+            child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessage(),
+          ],
+        )));
   }
 }
