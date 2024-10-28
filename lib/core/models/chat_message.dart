576d3de12c14ece7fa937e709e755cdf10ccbb85
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-10-28 18:30:35 -0400

    Criando o Chat Service

--- lib/core/models/chat_message.dart
+++ lib/core/models/chat_message.dart
@@ -9,11 +9,11 @@ class ChatMessage {
   final String userImageURL;
 
   const ChatMessage({
+    required this.id,
     required this.text,
     required this.createAt,
     required this.userId,
     required this.userName,
     required this.userImageURL,
-    required this.id,
   });
 }
