393085cd4c478e8187e986d524acb45008a77a5d
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-10-31 20:02:29 -0400

    Incluindo o avatar no balão de mensagem

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chat/core/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
@@ -8,39 +10,73 @@ class MessageBubble extends StatelessWidget {
   const MessageBubble(
       {super.key, required this.message, required this.belongsToMe});
 
+  Widget _showUserImage(String imageURL) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageURL);
+
+    if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment:
-          belongsToMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: belongsToMe ? Colors.green.shade200 : Colors.greenAccent,
-            borderRadius: BorderRadius.all(
-              Radius.circular(12),
+        Row(
+          mainAxisAlignment:
+              belongsToMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongsToMe ? Colors.green.shade200 : Colors.greenAccent,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft:
+                      belongsToMe ? Radius.circular(12) : Radius.circular(0),
+                  bottomRight:
+                      belongsToMe ? Radius.circular(0) : Radius.circular(12),
+                ),
+              ),
+              width: 180,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: belongsToMe
+                    ? CrossAxisAlignment.end
+                    : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    textAlign: belongsToMe ? TextAlign.right : TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          width: 180,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
+          ],
         ),
+        Positioned(
+          top: 0,
+          left: belongsToMe ? null : 165,
+          right: belongsToMe ? 165 : null,
+          child: _showUserImage(message.userImageURL),
+        )
       ],
     );
   }
