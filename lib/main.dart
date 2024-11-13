0a67ac99af08c3f9ad8f2a03b783a12c1ad5b298
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-11-12 20:05:51 -0400

    Autenticação com Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
